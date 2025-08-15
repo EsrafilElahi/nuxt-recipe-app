0db630bc806f5ceabf2ffe547215ddb63c3e433f
esrafil <elahi.esrafil@okala.com>
2025-08-16 01:49:19 +0330

    edit theme

--- app/assets/themes/myTheme.js
+++ app/assets/themes/myTheme.js
@@ -5,12 +5,12 @@ const MyPreset = definePreset(Aura, {
   semantic: {
     primary: {
       default: "#d0c",
-      50: "{yellow.50}",
-      100: "{yellow.100}",
-      200: "{yellow.200}",
-      300: "{yellow.300}",
+      50: "#d0c",
+      100: "#d0c",
+      200: "#d0c",
+      300: "#d0c",
       400: "{yellow.400}",
-      500: "{yellow.500}",
+      500: "#d0c",
       600: "{yellow.600}",
       700: "{yellow.700}",
       800: "{yellow.800}",
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,7 @@
   <div>
     <span class="text-primary-100 font-bold">home page</span>
 
-    <Button label="Verify" />
+    <Button label="Verify" color="primary-400" severity="primary" />
 
     <Button label="دکمه تست" icon="pi pi-check" />
     <InputText v-model="value" placeholder="متن..." />
